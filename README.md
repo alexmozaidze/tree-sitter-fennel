0016d73ffba756bc14bb82b9d038453ad639d5d5
Alexei Mozaidze <lexerpexer@proton.me>
2024-02-09 21:35:27 +0400

    feat!: big rewrite
    Simplified the design to only handle syntax items, rather than all the
    macros, builtins and whatnot. Made everything to be easily querryable,
    so there should be no problems with that.

--- README.md
+++ README.md
@@ -1,9 +1,134 @@
-![testing badge](https://github.com/travonted/tree-sitter-fennel/workflows/Testing/badge.svg)
+<div align="center">
 
-tree-sitter-fennel
-==================
 
-[Fennel](https://fennel-lang.org) grammar for [tree-sitter](https://github.com/tree-sitter/tree-sitter)
+# tree-sitter-fennel
 
-#### References
+[Fennel](https://fennel-lang.org) grammar for [tree-sitter](https://github.com/tree-sitter/tree-sitter) focused on simplicity.
+
+
+</div>
+
+Very few forms are baked into the grammar, but even then they appear as `(symbol)` for ease of querying and less confusion.
+
+For a full list of special symbols, see [`SPECIAL_OVERRIDE_SYMBOLS`](/grammar.js#L15-L24) at the top of the grammar file.
+
+## Syntax items
+
+This is a list of all the most important syntax items this parser produces
+
+This list contains explicit queries that describe where a syntax item may be used and also example queries.
+
+> [!NOTE]\
+> Some of the unimportant anonymous nodes were omitted for clarity purposes.
+
+### Compounds
+
+- `(list)` :: list literal `()`
+```racket
+(list
+  (_)*)
+```
+- `(sequence)` :: sequence literal, otherwise known as sequential table `[]`
+```racket
+(sequence
+  (_)*)
+```
+- `(table)` :: table literal `{}`
+```racket
+(table
+  (table_pair
+    key: (_)
+    value: (_))*)
+```
+
+### Primitives
+
+- `(string)` :: either colon string `:abc` or double quote string `"abc"`
+```racket
+;; colon string
+(string
+  ":"
+  (string_content))
+
+;; double quote string
+(string
+  "\""
+  (string_content
+    (escape_sequence)*)
+  "\"")
+```
+- `(number)` :: any valid number
+```racket
+(number)
+```
+- `(nil)` :: just `nil`
+```racket
+(nil)
+```
+- `(boolean)` :: either `true` or `false`
+```racket
+(boolean)
+```
+
+### Symbols
+
+- `(symbol)` :: anything that's not explicitly defined in the grammar is a symbol
+```racket
+(symbol)
+```
+- `(symbol_fragment)` :: same as `(symbol)`, but only used in multi-symbols
+- `(multi_symbol)` :: a comma separated mush of symbols `a.b.c.d`
+```racket
+(multi_symbol
+  (symbol_fragment)
+  (symbol_fragment)+)
+```
+- `(multi_symbol_method)` :: method call `a.b.c:deeznuts`, note that it's only allowed to appear as the first item of `(list)`
+```racket
+(list
+  .
+  (multi_symbol_method
+    base: (multi_symbol
+            (symbol_fragment)
+            (symbol_fragment)+)
+    method: (symbol_fragment))
+  (_)*)
+```
+
+### Reader macros
+
+Every reader macro is to be queried like so:
+```racket
+(reader_macro
+  macro: "#" ; put any other reader macro character here
+  expression: (_))
+```
+
+For a full list of valid reader macros see [`READER_MACRO_CHARS`](/grammar.js#L26-L31) in the grammar file.
+
+### Misc
+
+- `(shebang)` :: if the very first line starts with `#!`, it matches that line
+```racket
+(shebang)
+```
+
+### Special cases
+
+- `#` :: `#` is recognized as a reader macro if it's immediately followed by an expression, and a symbol otherwise
+```racket
+;; #hello
+(reader_macro
+  macro: "#"
+  expression: (symbol))
+
+;; (# my-table)
+(list
+  (symbol)
+  (symbol))
+```
+
+## References
+
 - [Fennel language reference](https://fennel-lang.org/reference)
+- [Original tree-sitter-fennel parser](https://github.com/TravonteD/tree-sitter-fennel)
--- grammar.js
+++ grammar.js
@@ -1,475 +1,153 @@
-const SYMBOL = choice(
+/**
+ * Get literal out of a syntax node
+ *
+ * Example: get_literal(prec(1, '#')) == '#'
+ *
+ * @param {Object} node
+ * @returns {string} Literal extracted from node
+ */
+function get_literal(node) {
+	if (typeof node === 'string' || node instanceof RegExp) return node;
+	else if (node.type === 'PATTERN' || node.type === 'STRING') return node.value;
+	else return get_literal(node.content);
+}
+
+// Symbols that should take priority over the default symbol definition
+const SPECIAL_OVERRIDE_SYMBOLS = [
+	'#',
+	'?.',
 	':',
-	seq('.', /[^(){}\[\]"'~;,@`\s]*/),
-	/[^#(){}\[\]"'~;,@`.:\s][^(){}\[\]"'~;,@`.:\s]*/,
-);
+	'$...',
+	'...',
+	'..',
+	prec.dynamic(-1, '.'),
+];
+
+const READER_MACRO_CHARS = [
+	prec(1, '#'),
+	'\'',
+	'`',
+	',',
+];
 
 module.exports = grammar({
 	name: 'fennel',
 
-	word: $ => $.symbol,
-
-	extras: $ => [
-		/\s/,
-		$.comment,
-	],
+	extras: $ => [],
 
 	conflicts: $ => [
-		[$.binding, $._sexp],
-		[$.binding, $.table],
-		[$.table_binding, $.table],
-		[$.sequential_table_binding, $.sequential_table],
+		[$.multi_symbol, $._sexp],
 	],
 
 	rules: {
-		program: $ => repeat($._sexp),
+		program: $ => seq(
+			optional($.shebang),
+			repeat(choice(
+				$._sexp,
+				$._gap,
+			))
+		),
+
+		shebang: $ => /#!.*/,
+
+		_whitespace: $ => /\s+/,
+		comment: $ => /;.*\n?/,
+		_gap: $ => choice(
+			$._whitespace,
+			$.comment,
+		),
 
 		_sexp: $ => choice(
-			$._special_form,
+			$._special_override_symbols,
+			$.reader_macro,
 			$.symbol,
 			$.multi_symbol,
 			$.list,
-			$.sequential_table,
+			$.sequence,
 			$.table,
 			$._literal,
 		),
 
-		_special_form: $ => choice(
-			$.fn,
-			$.lambda,
-			$.hashfn,
-			$.match,
-			$.let,
-			$.global,
-			$.local,
-			$.var,
-			$.set,
-			$.each,
-			$.collect,
-			$.icollect,
-			$.accumulate,
-			$.for,
-			$.quote,
+		_special_override_symbols: $ => alias(choice(...SPECIAL_OVERRIDE_SYMBOLS), $.symbol),
+
+		_reader_macro_char: $ => choice(...READER_MACRO_CHARS),
+
+		reader_macro: $ => seq(
+			field('macro', $._reader_macro_char),
+			field('expression', $._sexp),
 		),
 
-		each: $ => seq(
-			'(',
-			'each',
-			'[',
-			$.iter_bindings,
-			']',
-			repeat($._sexp),
-			')',
-		),
-
-		iter_bindings: $ => seq(
-			repeat($._binding),
-			field('iterator', $._sexp),
-			optional(seq(
-				':until',
-				field('until', $._sexp),
-			)),
-		),
-
-		for: $ => seq(
-			'(',
-			'for',
-			$.for_clause,
-			repeat($._sexp),
-			')',
-		),
-
-		for_clause: $ => seq(
-			'[',
-			$.symbol,
-			$._sexp,
-			$._sexp,
-			optional($._sexp),
-			']',
-		),
-
-		let: $ => seq(
-			'(',
-			'let',
-			$.let_clause,
-			repeat($._sexp),
-			')',
-		),
-
-		let_clause: $ => seq(
-			'[',
-			repeat(seq(
-				$._binding,
-				$._sexp,
-			)),
-			']',
-		),
-
-		global: $ => seq(
-			'(',
-			'global',
-			$._binding,
-			$._sexp,
-			')',
-		),
-
-		local: $ => seq(
-			'(',
-			'local',
-			$._binding,
-			$._sexp,
-			')',
-		),
-
-		var: $ => seq(
-			'(',
-			'var',
-			$._binding,
-			$._sexp,
-			')',
-		),
-
-		set: $ => seq(
-			'(',
-			'set',
-			$._assignment,
-			$._sexp,
-			')',
-		),
-
-		_binding: $ => choice(
-			$.multi_value_binding,
-			$._non_multi_value_binding,
-		),
-
-		multi_value_binding: $ => seq(
-			'(',
-			repeat($._non_multi_value_binding),
-			')',
-		),
-
-		_non_multi_value_binding: $ => choice(
-			$.binding,
-			$.sequential_table_binding,
-			$.table_binding,
-		),
-
-		binding: $ => $.symbol,
-
-		sequential_table_binding: $ => seq(
-			'[',
-			repeat($._non_multi_value_binding),
-			optional(seq('&', $.binding)),
-			']',
-		),
-
-		table_binding: $ => seq(
-			'{',
-			repeat(choice(
-				seq(':', $.binding),
-				seq('&as', $.binding),
-				seq($._sexp, $._non_multi_value_binding),
-			)),
-			'}',
-		),
-
-		_assignment: $ => choice(
-			$.multi_value_assignment,
-			$._non_multi_value_assignment,
-		),
-
-		multi_value_assignment: $ => seq(
-			'(',
-			repeat($._non_multi_value_assignment),
-			')',
-		),
-
-		_non_multi_value_assignment: $ => choice(
-			$.assignment,
-			$.sequential_table_assignment,
-			$.table_assignment,
-		),
-
-		assignment: $ => choice(
-			$.symbol,
-			$.multi_symbol,
-		),
-
-		sequential_table_assignment: $ => seq(
-			'[',
-			repeat($._non_multi_value_assignment),
-			']',
-		),
-
-		table_assignment: $ => seq(
-			'{',
-			repeat(choice(
-				seq(':', $.assignment),
-				seq($._sexp, $._non_multi_value_assignment),
-			)),
-			'}',
-		),
-
-		hashfn: $ => choice(
-			seq(
-				'(',
-				'hashfn',
-				repeat($._sexp),
-				')',
-			),
-			seq(
-				'#',
-				$._sexp,
-			),
-		),
-
-		fn: $ => seq(
-			'(',
-			'fn',
-			$._function_body,
-			')',
-		),
-
-		lambda: $ => seq(
-			'(',
-			choice('lambda', 'λ'),
-			$._function_body,
-			')',
-		),
-
-		_function_body: $ => prec(1, seq(
-			optional(field('name', choice(
-				$.symbol,
-				$.multi_symbol,
-			))),
-			$.parameters,
-			optional(seq(
-				optional(field('docstring', $.string)),
-				repeat1($._sexp),
-			)),
-		)),
-
-		parameters: $ => seq(
-			'[',
-			repeat(choice(
-				$._binding,
-				$.vararg,
-			)),
-			']',
-		),
-
-		match: $ => seq(
-			'(',
-			'match',
-			$._sexp,
-			repeat(seq(
-				$._pattern,
-				$._sexp,
-			)),
-			')',
-		),
-
-		_pattern: $ => choice(
-			$._simple_pattern,
-			$.where_pattern,
-			$.guard_pattern,
-		),
-
-		_simple_pattern: $ => choice(
-			$.multi_value_pattern,
-			$._non_multi_value_pattern,
-		),
-
-		guard_pattern: $ => seq(
-			'(',
-			$._simple_pattern,
-			'?',
-			field('guard', repeat1($._sexp)),
-			')',
-		),
-
-		where_pattern: $ => seq(
-			'(',
-			'where',
+		_list_content: $ => seq(
+			optional($._gap),
 			choice(
-				$._simple_pattern,
-				seq(
-					'(',
-					'or',
-					repeat($._simple_pattern),
-					')',
-				),
+				$.multi_symbol_method,
+				$._sexp,
 			),
-			field('guard', repeat($._sexp)),
-			')',
-		),
-
-		multi_value_pattern: $ => seq(
-			'(',
-			repeat($._non_multi_value_pattern),
-			')',
-		),
-
-		_non_multi_value_pattern: $ => choice(
-			$._literal,
-			$.symbol,
-			$.multi_symbol,
-			$.sequential_table_pattern,
-			$.table_pattern,
-		),
-
-		sequential_table_pattern: $ => seq(
-			'[',
-			repeat($._non_multi_value_pattern),
-			']',
-		),
-
-		table_pattern: $ => seq(
-			'{',
 			repeat(choice(
-				seq(
-					':',
-					$._simple_pattern,
-				),
-				seq(
-					$._sexp,
-					$._non_multi_value_pattern,
-				),
+				$._sexp,
+				$._gap,
 			)),
-			'}',
-		),
-
-		collect: $ => seq(
-			'(',
-			'collect',
-			'[',
-			$.iter_bindings,
-			']',
-			repeat($._sexp),
-			')',
-		),
-
-		icollect: $ => seq(
-			'(',
-			'icollect',
-			'[',
-			$.iter_bindings,
-			']',
-			repeat($._sexp),
-			')',
-		),
-
-		accumulate: $ => seq(
-			'(',
-			'accumulate',
-			'[',
-			$._binding,
-			$._sexp,
-			$.iter_bindings,
-			']',
-			repeat($._sexp),
-			')',
-		),
-
-		quote: $ => choice(
-			seq(
-				'(',
-				'quote',
-				$._quoted_sexp,
-				')',
-			),
-			seq(
-				choice('\'', '`'),
-				$._quoted_sexp,
-			),
-		),
-
-		unquote: $ => seq(
-			',',
-			$._sexp,
-		),
-
-		_quoted_sexp: $ => choice(
-			$.unquote,
-			$.symbol,
-			$.multi_symbol,
-			$.multi_symbol_method,
-			$.quoted_list,
-			$.quoted_sequential_table,
-			$.quoted_table,
-			$._literal,
-		),
-
-		quoted_list: $ => seq(
-			'(',
-			repeat($._quoted_sexp),
-			')',
-		),
-
-		quoted_sequential_table: $ => seq(
-			'[',
-			repeat($._quoted_sexp),
-			']',
-		),
-
-		quoted_table: $ => seq(
-			'{',
-			repeat($._quoted_sexp),
-			'}',
 		),
 
 		list: $ => seq(
-			'(',
-			choice(
+			field('open', '('),
+			optional($._list_content),
+			field('close', ')'),
+		),
+
+		sequence: $ => seq(
+			field('open', '['),
+			repeat(choice(
 				$._sexp,
-				$.multi_symbol_method,
-			),
-			repeat($._sexp),
-			')',
+				$._gap,
+			)),
+			field('close', ']'),
 		),
 
-		sequential_table: $ => seq(
-			'[',
-			repeat($._sexp),
-			']',
+		table_pair: $ => seq(
+			field('key', $._sexp),
+			$._gap,
+			field('value', $._sexp),
 		),
 
-		table_pair: $ => prec.left(1, choice(
-			seq(':', $.binding),
-			seq(
-				field('key', $._sexp),
-				field('value', $._sexp)
-			),
-		)),
-
-		table: $ => seq('{', repeat($.table_pair), '}'),
+		table: $ => seq(
+			field('open', '{'),
+			repeat(choice(
+				$.table_pair,
+				$._gap,
+			)),
+			field('close', '}'),
+		),
 
 		_literal: $ => choice(
 			$.string,
 			$.number,
 			$.boolean,
-			$.vararg,
 			$.nil,
-			$.nil_safe,
 		),
 
 		nil: $ => 'nil',
-		nil_safe: $ => '?.',
-		vararg: $ => '...',
-		boolean: $ => choice('true', 'false'),
+		boolean: $ => token(choice('true', 'false')),
 
-		_colon_string: $ => seq(
-			':',
-			alias(token.immediate(/[^(){}\[\]"'~;,@`\s]+/), $.string_content),
-		),
+		_colon_string: $ => prec(2, seq(
+			field('open', ':'),
+			alias(choice(
+				// HACK(alexmozaidze): This makes expressions such as `:?.` and `:#` parse correctly
+				// instead of being 2 separate `(symbol)` tokens. It would make sense to have an
+				// external scanner do this work, but I don't know how to write one yet.
+				// Besides, not using an external scanner means there will be less bugs.
+				...[...SPECIAL_OVERRIDE_SYMBOLS].map(get_literal),
+				/[^(){}\[\]"'~;,@`\s]+/,
+			), $.string_content),
+		)),
 
 		_double_quote_string: $ => seq(
-			'"',
+			field('open', '"'),
 			alias(repeat(choice(
-				token.immediate(prec(1, /[^"\\]+/)),
+				prec(1, /[^"\\]+/),
 				$.escape_sequence,
 			)), $.string_content),
-			'"',
+			field('close', '"'),
 		),
 
 		string: $ => choice(
@@ -477,7 +155,7 @@ module.exports = grammar({
 			$._double_quote_string,
 		),
 
-		escape_sequence: $ => token.immediate(seq(
+		escape_sequence: $ => token(seq(
 			'\\',
 			choice(
 				/[^xu\d]/,
@@ -520,181 +198,24 @@ module.exports = grammar({
 			));
 		},
 
-		// Normally, in a `seq` there can be any number of spaces (or anything
-		// that's in the `extras` array of your grammar) between the elements.
-		// However, in Fennel multi-syms cannot contain spaces.  That's why we use
-		// `token.immediate` in every element after the first one.	Unfortunately,
-		// it does not accept named rules which is why `identifier_immediate` exist.
-		// The whole thing is a bit of a hack really but it works.
-		//
-		// There's a draft for a general `immediate` rule (tree-sitter/tree-sitter#1102)
-		// Alternatively, we could stop using `extras` and instead explicitely
-		// allow whitespaces in rules.
-		multi_symbol: $ => seq(
-			$.symbol,
+		// NOTE: Due to special override symbol `.` we need dynamic precedence here
+		multi_symbol: $ => prec.dynamic(2, prec.right(seq(
+			alias($.symbol, $.symbol_fragment),
 			repeat1(seq(
-				token.immediate(prec(2, '.')),
-				alias($.symbol_immediate, $.symbol),
+				'.',
+				alias($.symbol, $.symbol_fragment),
 			)),
-		),
+		))),
 
-		multi_symbol_method: $ => seq(
-			choice(
-				$.symbol,
-				$.multi_symbol,
-			),
-			token.immediate(prec(2, ':')),
-			alias($.symbol_immediate, $.symbol),
-		),
+		multi_symbol_method: $ => prec(3, seq(
+		field('base', choice(
+			alias($.symbol, $.symbol_fragment),
+			$.multi_symbol,
+		)),
+		':',
+		field('method', alias($.symbol, $.symbol_fragment)),
+	)),
 
-		// In the compiler, a symbol is really anything that's left during parsing,
-		// i.e. anything that's not a number nor a string nor a table, etc.  There
-		// is no defined character set to match every symbol, just some characters
-		// that cannot be in one.  We impose some further restrictions ourselves,
-		// namely that it cannot contain a dot or a colon; we need to match those
-		// separately to find multi-syms.
-		//
-		// XXX: needs to be after number; we could try not matching numbers as
-		// starting characters but then we'd need to take into account the sign too
-		// as well as any underscores (numerical separators) which could even show
-		// up between the sign and the first digit and that's just messy e.g. +__10
-		// is a number
-		symbol: $ => token(SYMBOL),
-		symbol_immediate: $ => token.immediate(SYMBOL),
-
-		comment: $ => token(seq(';', /.*/)),
-
-		// _specials: $ => choice(
-		//	 $._macros,
-		//	 $._arithmetic_operator,
-		//	 $._bitwise_operator,
-		//	 $._boolean_operator,
-		//	 $._comparison_operator,
-		//	 $._string_operator,
-		//	 $._table_operator,
-		//	 'comment',
-		//	 'do',
-		//	 'doc',
-		//	 'each',
-		//	 'eval-compiler',
-		//	 'fn',
-		//	 'for',
-		//	 'global',
-		//	 'hashfn',
-		//	 'if',
-		//	 'include',
-		//	 'let',
-		//	 'local',
-		//	 'lua',
-		//	 'macros',
-		//	 'quote',
-		//	 'require-macros',
-		//	 'set',
-		//	 'tset',
-		//	 'values',
-		//	 'var',
-		//	 'while',
-		// ),
-
-		// _macros: $ => choice(
-		//	 '->',
-		//	 '->>',
-		//	 '-?>',
-		//	 '-?>>',
-		//	 '?.',
-		//	 'accumulate',
-		//	 'collect',
-		//	 'doto',
-		//	 'icollect',
-		//	 'import-macros',
-		//	 'lambda',
-		//	 'macro',
-		//	 'macrodebug',
-		//	 'match',
-		//	 'partial',
-		//	 'pick-args',
-		//	 'pick-values',
-		//	 'when',
-		//	 'with-open',
-		// ),
-
-		// _eval_compiler_scope: $ => choice(
-		//	 'assert-compile',
-		//	 'gensym',
-		//	 'list',
-		//	 'list?',
-		//	 'multi-sym?',
-		//	 'sequence?',
-		//	 'sym',
-		//	 'sym?',
-		//	 'table?',
-		//	 'varg?',
-		// ),
-
-		// _macro_scope: $ => choice(
-		//	 $._eval_compiler_scope,
-		//	 'in-scope?',
-		//	 'macroexpand',
-		// ),
-
-		// _arithmetic_operator: $ => choice('+', '-', '*', '%', '/', '//', '^'),
-		// _bitwise_operator: $ => choice('<<', '>>', '&', '|', '~', 'bnot'),
-		// _boolean_operator: $ => choice('and', 'or', 'not'),
-		// _comparison_operator: $ => choice('>', '<', '>=', '<=', '=', 'not='),
-		// _string_operator: $ => choice('..', 'length'),
-		// _table_operator: $ => choice('.', ':'),
-
-		// _lua_packages: $ => choice(
-		//	 'coroutine',
-		//	 'debug',
-		//	 'io',
-		//	 'math',
-		//	 'os',
-		//	 'package',
-		//	 'string',
-		//	 'table',
-		//	 'utf8',
-		// ),
-
-		// _lua_builtins: $ => choice(
-		//	 $._lua_packages,
-		//	 $._lua51_builtins,
-		//	 'arg',
-		//	 'assert',
-		//	 'collectgarbage',
-		//	 'dofile',
-		//	 '_ENV',
-		//	 'error',
-		//	 '_G',
-		//	 'getmetatable',
-		//	 'ipairs',
-		//	 'load',
-		//	 'loadfile',
-		//	 'next',
-		//	 'pairs',
-		//	 'pcall',
-		//	 'print',
-		//	 'rawequal',
-		//	 'rawget',
-		//	 'rawlen',
-		//	 'rawset',
-		//	 'require',
-		//	 'select',
-		//	 'setmetatable',
-		//	 'tonumber',
-		//	 'tostring',
-		//	 'type',
-		//	 '_VERSION',
-		//	 'warn',
-		//	 'xpcall',
-		// ),
-
-		// _lua51_builtins: $ => choice(
-		//	 'loadstring',
-		//	 'module',
-		//	 'setfenv',
-		//	 'unpack',
-		// ),
-
+		symbol: $ => /[^#(){}\[\]"'~;,@`.:\s][^(){}\[\]"'~;,@`.:\s]*/,
 	},
 });
